f9923af96ce6034adc7f706f9e7b599c497513f6
fx196 <mikechen212@gmail.com>
2020-06-02 20:30:12 -0400

    add prints for dag create

--- cloudburst-cpp/src/scheduler/dag_create_handler.cpp
+++ cloudburst-cpp/src/scheduler/dag_create_handler.cpp
@@ -19,6 +19,8 @@ void dag_create_handler(string serialized, zmq::socket_t &dag_create_socket, Soc
     Dag dag;
     dag.ParseFromString(serialized);
 
+    std::cout << "recieved dag create request" << std::endl;
+
     // We do not allow duplicate DAG names, so we return an error if the name is already taken
     if(dags.find(dag.name()) != dags.end()){
         GenericResponse error;
@@ -29,6 +31,8 @@ void dag_create_handler(string serialized, zmq::socket_t &dag_create_socket, Soc
         kZmqUtil->send_string(serialized, &dag_create_socket);
         return;
     }
+
+    std::cout << "Creating dag " << dag.name() << std::endl;
 
     log->info("Creating DAG {}.", (dag.name()));
 
@@ -52,6 +56,8 @@ void dag_create_handler(string serialized, zmq::socket_t &dag_create_socket, Soc
         }
     }
 
+    std::cout << "Dag functions pinned" << std::endl;
+
     // We persist the DAG in the KVS, so other schedulers can read the DAG when they hear about it.
     LWWPairLattice<string> payload(TimestampValuePair<string>(generate_timestamp(0), serialized));
     kvs_put(kvs, dag.name(), serialize(payload), log, LatticeType::LWW);
@@ -67,6 +73,8 @@ void dag_create_handler(string serialized, zmq::socket_t &dag_create_socket, Soc
     policy->commit_dag(dag.name());
     pair<Dag, set<string>> dag_sources_pair(dag, find_dag_source(dag));
     dags.insert(pair<string, pair<Dag, set<string>>>(dag.name(), dag_sources_pair));
+
+    std::cout << "Committing dag" << std::endl;
 
     // Send ok response
     GenericResponse response;
